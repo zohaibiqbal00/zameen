4ec642d38190224893625c7b818eb733c130e335
rafia_rasheed <rafiarasheed64@gmail.com>
2024-02-01 22:09:39 +0500

    added tab comp

--- zameen.com/app/page.tsx
+++ zameen.com/app/page.tsx
@@ -3,12 +3,14 @@ import Nav from './Components/Navbar/page'
 import Header from './Components/Header/page'
 import Properties from './Components/Properties/page'
 import Footer from './Components/Footer/page';
+import Tabs from './Components/Tabs/page'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
       <Nav />
       <Properties />
+      <Tabs/>
       <Footer/>
     </main>
   );

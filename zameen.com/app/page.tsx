c2921400a5a54f53d73128b986ad8b09fd048dd5
rafia_rasheed <rafiarasheed64@gmail.com>
2024-02-01 19:29:52 +0500

    add footer

--- zameen.com/app/page.tsx
+++ zameen.com/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Nav from './Components/Navbar/page'
 import Header from './Components/Header/page'
 import Properties from './Components/Properties/page'
+import Footer from './Components/Footer/page';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header />
       <Nav />
       <Properties />
-
-
+      <Footer/>
     </main>
   );
 }

4d14cdbd0eb75abe87c42b631728b711dc60079c
Arslan-Abrahum <abrahum9328@gmail.com>
2024-02-05 14:44:41 +0500

    Added New Comp

--- zameen.com/app/page.tsx
+++ zameen.com/app/page.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 import Nav from './Components/Navbar/page'
 import Header from './Components/Header/page'
 import Properties from './Components/Properties/page'
-import Slider from './Components/Slider/page'
 import Footer from './Components/Footer/page';
 import Tabs from './Components/Tabs/page'
+import Sli from "./Components/Sli/page"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       <Header />
       <Nav />
       <Properties />
-      <Slider/>
+      <Sli />
       <Tabs/>
       <Footer/>
     </main>

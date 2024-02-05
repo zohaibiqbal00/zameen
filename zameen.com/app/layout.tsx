57b8176a49914a3085cec21a8c20b60d7eb94ed0
Arslan-Abrahum <abrahum9328@gmail.com>
2024-02-05 14:37:42 +0500

    Changed

--- zameen.com/app/layout.tsx
+++ zameen.com/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"  rel="stylesheet" />
+      </head>
+      <body className={inter.className}>{children}
+      
+      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+      </body>
     </html>
   );
 }
--- zameen.com/app/page.tsx
+++ zameen.com/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Nav from './Components/Navbar/page'
 import Header from './Components/Header/page'
 import Properties from './Components/Properties/page'
+import Slider from './Components/Slider/page'
 import Footer from './Components/Footer/page';
 import Tabs from './Components/Tabs/page'
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <Nav />
       <Properties />
+      <Slider/>
       <Tabs/>
       <Footer/>
     </main>

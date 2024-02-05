1181cb954c211fe75f7c45471f86cac207ad2ee3
rafia_rasheed <rafiarasheed64@gmail.com>
2024-02-05 14:06:44 +0500

    slider comp

--- zameen.com/app/Components/Slider/page.tsx
+++ zameen.com/app/Components/Slider/page.tsx
@@ -1,96 +1,83 @@
-"use client"
+  "use client"
 
 
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
+  import {Swiper, SwiperSlide} from "swiper/react"
+  import 'swiper/css';
 
-const Page: React.FC = () => {
-  const [slide, setSlide] = useState<number>(0);
+  const Page: React.FC = () => {
 
-  return (
-    <div className='bg-red-400'>
-      <div className="slider-container">
-        <div className="slider flex" style={{ transform: `translateX(${slide * 100}%)` }}>
-          <div className="card p-4 mx-2 bg-white shadow-lg text-black">
-            {/* <p>Card 1 Content</p> */}
 
-            <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
-      <img className="w-full h-48 object-cover" src="https://zameen-strapi-live.s3.eu-west-1.amazonaws.com/0_51d598366d.jpg" alt="" />
-       <div className="p-4">
-         <h2 className="text-xl font-semibold mb-2">PKR 1.05 Crore to 2.6 Crore Prime City Homes, Faisalabad</h2>
-         <p className="text-gray-600">Faisalabad,Satiana Road</p>
-         <p>House</p>
-         <p>675 sqft to 1600 sqft</p>
-       </div>
-     </div>
+    return (
+      <div className='bg-white w-full text-black p-8'>
+        <h2 className='mb-5 font-semibold text-3xl'>Zameen Project</h2>
+        <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className="max-w-sm mx-auto bg-white shadow-md rounded-md overflow-hidden">
+  <img className="w-full h-48 object-cover rounded-lg" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSRqRnFOVLUERuPzdhYj0UyZnriodHiaJyxbp-FLzumWtvQIliJ" alt="Card Image"/>
+  <div>
+  <h2 className="text-lg font-semibold text-gray-800">PKR 79.79 lakh to 2.4 crore box park 11,Rawalpindi</h2>
+    <p className="text-gray-600 mt-2">Rawalpindi,Bahria Town Rawalpindi</p>
+    <p className='font-bold'>Shops</p>
+    <p>121 sqft to 467 sqft</p>
+  </div>
+</div>
+</SwiperSlide>
+        <SwiperSlide>
+        <div className="max-w-sm mx-auto bg-white shadow-md rounded-md overflow-hidden">
+  <img className="w-full h-48 object-cover rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyKJMcGUoKoFmKwAWKI18kr2fnBbJNwCOdxpKjagMhI978wPVd" alt="Card Image"/>
+  <div>
+  <h2 className="text-lg font-semibold text-gray-800">PKR 79.79 lakh to 2.4 crore box park 11,Rawalpindi</h2>
+    <p className="text-gray-600 mt-2">Rawalpindi,Bahria Town Rawalpindi</p>
+    <p className='font-bold'>Shops</p>
+    <p>121 sqft to 467 sqft</p>
+  </div>
+</div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="max-w-sm mx-auto bg-white shadow-md rounded-md overflow-hidden">
+  <img className="w-full h-48 object-cover rounded-lg" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR-ZtoawbJ6Cjq1ijHwwgRTYyCQ9ZO0TX9-G_BWHhoS96U-TKjD" alt="Card Image"/>
+  <div>
+  <h2 className="text-lg font-semibold text-gray-800">PKR 79.79 lakh to 2.4 crore box park 11,Rawalpindi</h2>
+    <p className="text-gray-600 mt-2">Rawalpindi,Bahria Town Rawalpindi</p>
+    <p className='font-bold'>Shops</p>
+    <p>121 sqft to 467 sqft</p>
+  </div>
+</div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="max-w-sm mx-auto bg-white shadow-md rounded-md overflow-hidden">
+  <img className="w-full h-48 object-cover rounded-lg" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQnYNy4tBg6yYIK8X_M_48o9hM9S5Sr58k-XtQr9dhZlopxh-xF" alt="Card Image"/>
+  <div>
+    <h2 className="text-lg font-semibold text-gray-800">PKR 79.79 lakh to 2.4 crore box park 11,Rawalpindi</h2>
+    <p className="text-gray-600 mt-2">Rawalpindi,Bahria Town Rawalpindi</p>
+    <p className='font-bold'>Shops</p>
+    <p>121 sqft to 467 sqft</p>
+  </div>
+</div>
+        </SwiperSlide>
 
-          </div>
-
-          <div className="card p-4 mx-2 bg-white shadow-lg text-black">
-            {/* <p>Card 2 Content</p> */}
-            <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
-      <img className="w-full h-48 object-cover" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSepngnnNrz1vBSERwttBVSx9nzgPZ1jqd_uBYLxtz59jnITLgk" alt="" />
-       <div className="p-4">
-         <h2 className="text-xl font-semibold mb-2">PKR 1.05 Crore to 2.6 Crore Prime City Homes, Faisalabad</h2>
-         <p className="text-gray-600">Faisalabad,Satiana Road</p>
-         <p>House</p>
-         <p>675 sqft to 1600 sqft</p>
-       </div>
-     </div>
-          </div>
-
-          <div className="card p-4 mx-2 bg-white shadow-lg text-black">
-            {/* <p>Card 1 Content</p> */}
-
-            <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
-      <img className="w-full h-48 object-cover" src="https://images.zameen.com/w1600_h900/7/1969/business_hub_35085.jpg" alt="" />
-       <div className="p-4">
-         <h2 className="text-xl font-semibold mb-2">PKR 1.05 Crore to 2.6 Crore Prime City Homes, Faisalabad</h2>
-         <p className="text-gray-600">Faisalabad,Satiana Road</p>
-         <p>House</p>
-         <p>675 sqft to 1600 sqft</p>
-       </div>
-     </div>
-          </div>
-
-          <div className="card p-4 mx-2 bg-white shadow-lg text-black">
-            {/* <p>Card 1 Content</p> */}
-            <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
-      <img className="w-full h-48 object-cover" src="https://images.zameen.com/w1600_h900/7/2173/platinum_homes_dham_38695.jpg" alt="" />
-       <div className="p-4">
-         <h2 className="text-xl font-semibold mb-2">PKR 1.05 Crore to 2.6 Crore Prime City Homes, Faisalabad</h2>
-         <p className="text-gray-600">Faisalabad,Satiana Road</p>
-         <p>House</p>
-         <p>675 sqft to 1600 sqft</p>
-       </div>
-     </div>
-          </div>
-
-          {/* <div className="card p-4 mx-2 bg-white shadow-lg text-black">
-            <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
-      <img className="w-full h-48 object-cover" src="https://images.zameen.com/w1600_h900/7/2360/prime_city_45115.jpg" alt="" />
-       <div className="p-4">
-         <h2 className="text-xl font-semibold mb-2">PKR 1.05 Crore to 2.6 Crore Prime City Homes, Faisalabad</h2>
-         <p className="text-gray-600">Faisalabad,Satiana Road</p>
-         <p>House</p>
-         <p>675 sqft to 1600 sqft</p>
-       </div>
-     </div>
-          </div> */}
-        </div>
+        <SwiperSlide>
+        <div className="max-w-sm mx-auto bg-white shadow-md rounded-md overflow-hidden">
+  <img className="w-full h-48 object-cover rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpt6N3xzmXBcOS_i_pOenN8ijj93VpJ6F8m87wgnoZGuXxB9SV" alt="Card Image"/>
+  <div>
+    <h2 className="text-lg font-semibold text-gray-800">PKR 79.79 lakh to 2.4 crore box park 11,Rawalpindi</h2>
+    <p className="text-gray-600 mt-2">Rawalpindi,Bahria Town Rawalpindi</p>
+    <p className='font-bold'>Shops</p>
+    <p>121 sqft to 467 sqft</p>
+  </div>
+</div>
+        </SwiperSlide>
+      </Swiper>
       </div>
+    );
+  };
 
-      <div className="mt-4 flex justify-between items-center">
-        <button onClick={() => setSlide((prevSlide) => (prevSlide - 1 + 2) % 2)} className="px-4 py-2 bg-blue-500 text-white rounded">
-          Previous
-        </button>
-        <button onClick={() => setSlide((prevSlide) => (prevSlide + 1) % 2)} className="px-4 py-2 bg-blue-500 text-white rounded">
-          Next
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default Page;
+  export default Page;
 
 
